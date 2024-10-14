8e500f90b411a4db4917a597a797bf8a2eda3a19
yamautigaito <gaito.y-0220@i.softbank.jp>
2024-10-14 15:23:47 +0900

    This commit implements a function in which tm_G's radius for each layer are gotten.

--- Assets/Project/Script/Searcher3.cs
+++ Assets/Project/Script/Searcher3.cs
@@ -11,7 +11,7 @@ public class Searcher3 : MonoBehaviour
 
     private float ReturnCorrectRadius(float y){
         //4-5の際の半径
-        float [] trueRadius={0.30379696284823254f,0.25485215839600045f,0.21799613961084838f,0.18262210691976397f,0.16326623536043514f};
+        float [] trueRadius={0.3232192f,0.2661974f,0.2253503f,0.1690026f,0.1804611f};
         //4-5の際の層ごとの高さ
         float [] trueHigh={0.3345878f,0.5892965f,0.8675417f,1.15422f,1.48762f};
         float radius=0.0f;
@@ -141,11 +141,92 @@ public class Searcher3 : MonoBehaviour
             Debug.Log("Layer" +i.ToString()+ "'s high  : "+tm_g_layers_y[i].ToString());
         }
 
+        //x,z座標の取得用配列
+        float[] LayerXpositions;
+        float[] LayerZpositions;
+
+        //配列長さはレイヤーが上下の柱より構成されるため2倍
+        LayerXpositions= new float [numPrism*2];
+        LayerZpositions= new float [numPrism*2]; 
+        //試行回数
+        int operateFor=0;
+        //一番長い距離
+        float maxDistance=0;
+
+        //直径の記録
+        float[] diameters;
+        diameters=new float [numLayer];
+
+        for(int i=0;i<numLayer;i++){
+
+            //下から生えている柱からend1の取得する
+            for(int j=0;j<numPrism;j++){
+                Debug.Log(j+i*numPrism);
+                strut=tm_g.transform.Find("Strut"+(j+i*numPrism).ToString()).gameObject;
+                
+                if(i==0){
+                    //baseStrutなので，end1ではなくendのみ
+                    end=strut.transform.Find("end");
+                }
+                else{
+                    //Strutのためend1
+                    end=strut.transform.Find("end1");
+                }
+                //比較するため配列に代入
+                LayerXpositions[j]=end.transform.position.x;
+                LayerZpositions[j]=end.transform.position.z;
+                //Debug.Log("i"+i.ToString()+",j"+j.ToString()+",Strut"+(j*i).ToString()+",tm_g_y"+end.transform.position.y.ToString());
+            }
+            
+             
+            //上から刺さっている柱からend2を取得する(一番上は除外)
+            if(i==(numLayer-1)){
+                operateFor=numPrism;
+            }
+            else{
+                for(int j=0;j<numPrism;j++){
+                    Debug.Log(j+(i+1)*numPrism);
+                    strut=tm_g.transform.Find("Strut"+(j+(i+1)*numPrism).ToString()).gameObject;
+                    end=strut.transform.Find("end2");
+
+                    LayerXpositions[j+numPrism]=end.transform.position.x;
+                    LayerZpositions[j+numPrism]=end.transform.position.z;
+                    //Debug.Log("i"+i.ToString()+",j"+j.ToString()+",Strut"+(j*i).ToString()+",tm_g_y"+end.transform.position.y.ToString());
+                }
+                operateFor=numPrism*2;
+                
+            }
+            
+            for(int j=0;j<operateFor;j++){
+                
+                //XとZのそれぞれの距離
+                tm_g_x=(float)Math.Pow((double)(LayerXpositions[0]-LayerXpositions[j]),2);
+                tm_g_z=(float)Math.Pow((double)(LayerZpositions[0]-LayerZpositions[j]),2);
+
+                if(maxDistance<=(float)(Math.Sqrt(tm_g_x+tm_g_z))){
+                    maxDistance=(float)Math.Sqrt(tm_g_x+tm_g_z);
+                }
+            }
+
+            
+            
+            
+            Debug.Log(i.ToString());
+            //配列にそれぞれを格納
+            diameters[i]=maxDistance;
+            maxDistance=0;
+
+            Array.Fill(LayerXpositions,0.0f);
+            Array.Fill(LayerZpositions,0.0f);
+
+        }
+
+        for(int i=0;i<numLayer;i++){
+            Debug.Log("Layer" +i.ToString()+ "'s radius  : "+diameters[i].ToString());
+        }
+        
 
         
-        
-
-
 
     }
 
