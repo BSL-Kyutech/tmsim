35c50d56ac1af595f860b2111f7fd9e644efa967
yamautigaito <gaito.y-0220@i.softbank.jp>
2024-10-18 17:34:33 +0900

    Add function ReturnCorrectHigh (in searcher3)

--- Assets/Project/Script/Searcher3.cs
+++ Assets/Project/Script/Searcher3.cs
@@ -50,6 +50,48 @@ public class Searcher3 : MonoBehaviour
         return radius;
     }
 
+    private float ReturnCorrectHigha(int i,int numLayer){
+        //4-5の際の層ごとの高さ
+        float [] trueHigh={0.3345878f,0.5892965f,0.8675417f,1.15422f,1.48762f};
+        float radius=0.0f;
+        float gradient=0.0f;
+        float intercept=0.0f;
+
+        //今の層がどこに位置するか調べる
+        int classifier=numLayer/5;
+
+        //それぞれの中間の高さを一次関数で近似
+        if(i<classifier){
+            gradient=(trueHigh[0]-0.0f)/(classifier);
+            intercept=0.0f;
+            radius=gradient*i+intercept;
+        }
+        else if(i<(classifier*2)){
+            gradient=(trueHigh[1]-trueHigh[0])/(classifier);
+            intercept=trueHigh[0];
+            radius=gradient*i+intercept;
+        }
+        else if(i<(classifier*3)){
+            gradient=(trueHigh[2]-trueHigh[1])/(classifier);
+            intercept=trueHigh[1];
+            radius=gradient*i+intercept;
+        }
+        else if(i<(classifier*4)){
+            gradient=(trueHigh[3]-trueHigh[2])/(classifier);
+            intercept=trueHigh[2];
+            radius=gradient*i+intercept;
+        }
+        else{
+            gradient=(trueHigh[4]-trueHigh[3])/(classifier);
+            intercept=trueHigh[3];
+            radius=gradient*i+intercept;
+        }
+        
+        
+        
+        return radius;
+    }
+
     //座標取得
     private (float[] LayerXpositions,float[] LayerYpositions,float[] LayerZpositions,int arrayLength) getPosition(int layerNum,int numPrism,int numLayer){
         
