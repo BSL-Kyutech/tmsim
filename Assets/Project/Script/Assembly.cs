c8d47bf4028d64a52edab368dfa8ee7f8908f13d
yamautigaito <gaito.y-0220@i.softbank.jp>
2024-10-28 11:03:46 +0900

    renovate Initializer add edgeFlag reset (in Assembly.cs)

--- Assets/Project/Script/Assembly.cs
+++ Assets/Project/Script/Assembly.cs
@@ -349,8 +349,9 @@ public class Assembly : MonoBehaviour
             //edgeLoop=new float[10]{0.065649f, 0.06289442f, 0.0731718f, 0.04244859f, 0.04472593f, 0.0510033f, 0.05128067f, 0.04755802f, 0.04683538f, 0.03011275f};
             //edgeLoop=new float[10]{0.065149f, 0.07389444f, 0.0701718f, 0.0634491f, 0.05972644f, 0.0550038f, 0.05028115f, 0.0455585f, 0.04183586f, 0.03461323f};
             initializer=false;
-            if(PlayerPrefs.HasKey("compareFlag"))
             PlayerPrefs.SetInt("compareFlag",0);
+            PlayerPrefs.Save();
+            PlayerPrefs.SetInt("EdgeFlag",0);
             PlayerPrefs.Save();
         }
         //edgeLoop= new float[10] {0.05733649f,0.0482649f,0.0458912f,0.04588119f,0.04587119f,0.02872325f,0.02871325f,0.02873325f,0.02870325f,0.02871325f};
