fe7ef337bdedb97b054c2d7ffeffa5f55583ca62
yamautigaito <gaito.y-0220@i.softbank.jp>
2024-10-29 13:13:15 +0900

    Add time.timeScale (in Assembly.cs)

--- Assets/Project/Script/Assembly.cs
+++ Assets/Project/Script/Assembly.cs
@@ -227,6 +227,7 @@ public class Assembly : MonoBehaviour
     // Start is called before the first frame update        最初の初期起動時の処理
     void Start()
     {
+        Time.timeScale=60.0f;
 
         //radiusLoopが内部に存在していた場合取得
         
