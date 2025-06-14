46d775e722e8522240f01b39c4595c977beb2c95
yamautigaito <gaito.y-0220@i.softbank.jp>
2025-06-14 12:43:27 +0900

    Add シリンダへの入力をランダムにする機能の追加 Add シリンダへの入力を固定値にする機能の追加

--- Assets/Project/Script/Demo.cs
+++ Assets/Project/Script/Demo.cs
@@ -17,13 +17,24 @@ public class Demo : MonoBehaviour
     //ばね定数の変更を自分でやるための変数
     public bool randomInput;
     public bool manualInput;
-    //度のシリンダーへやるか．
+    //manual Input　どのシリンダーへ入力するか．
     public int cylinderNum;
     public float sprringInput=0.0f;
 
     public bool standUp;
 
+    //breakingMode(一部破損時状態で行う際のモード)
+    public bool breaking_Mode;
+    public int breakNum;
+    public float BrokenInput=0;
+    public bool ConstantBroken=true;
+
+
     System.Random r = new System.Random(42);
+
+
+    //破壊されたシリンダの記録
+    private bool[] brokenCylinder;
 
     // Start is called before the first frame update
     void Start()
@@ -42,19 +53,51 @@ public class Demo : MonoBehaviour
             omega[i] = 1.2;
             //Debug.Log(omega[i]);
         }
+
+        //破壊扱いのシリンダの記憶配列
+        brokenCylinder=new bool[dev.numLayer*dev.numPrism*2];
+        for(int i=0;i<(dev.numLayer*dev.numPrism*2);i++){
+            brokenCylinder[i]=false;
+        }
     }
 
     // FixedUpdate is called once per physical simulation step
     void FixedUpdate()
     {
+
+        //壊れた状態のシリンダの記憶
+        if(breaking_Mode){
+            brokenCylinder[breakNum]=true;
+        }
+        //長い配列(dev.inputと同じ長さ)を用意 bool型
+        //リセット用モードをつくる
+        
+
         
         if (isActive) {
             for (int i = 0; i < dev.numLayer; i++) {
                 for (int j = 0; j < 2*dev.numPrism; j++) {
+                    
                     input[i*2*dev.numPrism + j] = Math.Cos(theta[i] + (Math.PI/dev.numPrism)*j);
-                    //COSでマイナスに行っても大丈夫なように ゆえに0.5が最大値になっているわけか.
+                    //COSでマイナスに行っても大丈夫なように 0.5が最大値になっているわけか.
                     input[i*2*dev.numPrism + j] = (input[i*2*dev.numPrism + j] + 1f)/2f;
-                    dev.input[i*2*dev.numPrism + j] = (float)input[i*2*dev.numPrism + j];
+
+
+                    if(breaking_Mode && brokenCylinder[i*2*dev.numPrism + j]==true){
+                        if(ConstantBroken){
+                            dev.input[i*2*dev.numPrism + j] = BrokenInput;
+                        }
+                        //random入力
+                        else{
+                            dev.input[i*2*dev.numPrism + j] = (float)(1 + r.NextDouble());
+                        }
+                        
+                    }
+                    else{
+                        dev.input[i*2*dev.numPrism + j] = (float)input[i*2*dev.numPrism + j];
+                    }
+
+                    
                 }
                 theta[i] += omega[i]*Time.fixedDeltaTime;
             }
@@ -76,6 +119,8 @@ public class Demo : MonoBehaviour
                 }
                 */
                 input[i] = r.NextDouble();
+
+                
                 dev.input[i]=(float)input[i];
                 
                 //Debug.Log(omega[i]);
@@ -120,5 +165,8 @@ public class Demo : MonoBehaviour
             standUp=false;
             
         }
+
+        
+        
     }
 }
